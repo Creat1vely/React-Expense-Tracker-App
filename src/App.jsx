536ca2ccf658331d138a2903ff3a25b40f7d9650
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-07-23 23:19:50 +0100

    mark: Added app's scripting and functionality
    - added the app's function and scripting giving life and
      usuablity to the app
    - added lot of comments to improve code readablity as
      code was just being written with no consideration for
      comments
    - preparing app for modularization and deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,250 @@
+import { useState } from "react"
 
 
 function App() {
+  // define app display state
+  const [ isExpenseFormVisible, setIsExpenseFormVisible ] = useState( false );
+
+  // define input states
+  const [ amount, setAmount ] = useState(0);
+  const [ desc, setDesc ] = useState("");
+  const [ type, setType ] = useState("Expense");
+  const [ searchValue, setSearchValue ] = useState("")
+
+  // define app's data
+  const [ transactions, setTransactions ] = useState([])
+
+  // addTransaction()
+  // used to add new transactions to the transactions state data
+  // on expense-form submit
+  function addTransaction(e) {
+    // prevent default form submit action
+    e.preventDefault();
+
+    // perform check to see if transaction data is valid
+    // and alert user if invalid else add new transaction
+    if ( amount != 0 && desc != "" ) {
+      // add new transaction
+      setTransactions([
+        ...transactions,
+        {
+          amount: amount,
+          desc: desc,
+          type: type,
+          id: Date.now()
+        }
+      ])
+    } else {
+      // alert user stating invalid data
+      alert("Enter a valid amount or description to add expense");
+    }
+  }
+
+  // getTotalIncome()
+  // used to get the total income of income-type transactions
+  // based on the argument passed to it ( which will be the 
+  // transactions state data )
+  function getTotalIncome( transactions ) {
+    var totalIncome = 0;
+    var incomeTransactions = transactions.filter( function( transaction ) {
+      if ( transaction.type == "Income" ) {
+        return true
+      }
+    })
+
+    incomeTransactions.forEach( function( transaction ) {
+      totalIncome += transaction.amount
+    })
+
+    return totalIncome;
+  }
+  
+  // getTotalExpense()
+  // used to get the total expense of expense-type transactions
+  // based on the argument passed to it ( which will be the 
+  // transactions state data )
+  function getTotalExpense( transactions ) {
+    var totalExpense = 0;
+    var expenseTransactions = transactions.filter( function( transaction ) {
+      if ( transaction.type == "Expense" ) {
+        return true
+      }
+    })
+
+    expenseTransactions.forEach( function( transaction ) {
+      totalExpense += transaction.amount
+    })
+
+    return totalExpense;
+  }
+
+
+
   return (
     <div className='app'>
+      {/* app title */}
       <h1 className="app-title">
         Expense Tracker
       </h1>
 
+      {/* expense tracker overview container */}
       <div className="overview">
 
         {/* balance container */}
         <div className="balance-ctn">
+
+          {/* balance */}
           <span className="balance">
-            balance: $10000
+            balance: ${ getTotalIncome( transactions ) - getTotalExpense( transactions) }
           </span>
 
-          <button className="add-button">
-            add
+          {/* expense tracker add-expense button */}
+          <button className="add-button" onClick={
+            ( isExpenseFormVisible ) ? 
+              () => { setIsExpenseFormVisible( false ) } :
+              () => { setIsExpenseFormVisible( true ) }
+          }>
+            { ( isExpenseFormVisible ) ? "cancel" : "add" }
           </button>
         </div>
 
         {/* expense overview container */}
         <div className="expense-overview-ctn">
+
+          {/* total expense text */}
           <div className="tile">
             <span className="tile-title">
               expense
             </span>
             <span className="tile-amount expense">
-              $7000
+              ${ getTotalExpense( transactions) }
             </span>
           </div>
           
+          {/* total income text */}
           <div className="tile">
             <span className="tile-title">
               income
             </span>
             <span className="tile-amount income">
-              $10000
+              ${ getTotalIncome( transactions ) }
             </span>
           </div>
         </div>
 
         {/* expense addition form */}
-        <form className="expense-form">
-          <input type="text" className="expense-form-input" placeholder="Amount"/>
-          <input type="text" className="expense-form-input" placeholder="Description"/>
+        { isExpenseFormVisible && <form className="expense-form" onSubmit={ addTransaction }>
+          {/* amount input */}
+          <input type="number" className="expense-form-input" 
+            placeholder="Amount" value={ amount } onChange={ ( e ) => { setAmount( e.target.valueAsNumber ) } }/>
+          
+          {/* desc input */}
+          <input type="text" className="expense-form-input" 
+            placeholder="Description" value={ desc } onChange={ ( e ) => { setDesc( e.target.value ) } }/>
 
+          {/* expense type radio button */}
           <div>
-            <input type="radio" name="expense-type" id="expense-radio" className="expense-form-radio"/>
+            <input type="radio" name="expense-type" id="expense-radio" className="expense-form-radio"
+              checked={ ( type == "Expense") } onChange={ () => { setType("Expense") }}/>
             <label htmlFor="expense-radio" className="expense-form-label">
               expense
             </label>
-            <input type="radio" name="expense-type" id="income-radio" className="expense-form-radio"/>
+            <input type="radio" name="expense-type" id="income-radio" className="expense-form-radio"
+              checked={ ( type == "Income") } onChange={ () => { setType("Income") }}/>
             <label htmlFor="income-radio" className="expense-form-label">
               income
             </label>
           </div>
 
+          {/* form "add transaction" submit */}
           <input type="submit" value="add transaction" className="add-button" />
-        </form>
+        </form>}
       </div>
 
       {/* transactions list container */}
       <div className="transactions-list-ctn">
 
         {/* transaction search input */}
-        <input type="text" className="transactions-search" placeholder="search items"/>
+        <input type="text" className="transactions-search" placeholder="search items"
+          value={ searchValue } onChange={ ( e ) => { setSearchValue( e.target.value ) }}/>
 
         {/* transactions list */}
         <div className="transactions-list">
 
-          <div className="transaction credit">
-            <span className="transaction-name">
-              Salary
-            </span>
+          {/* render list of transactions based on values in transaction data */}
+          {
+            searchValue == "" && transactions.map( function( transaction ) {
+              // check transaction type and return appropriate jsx markup based on the type
+              if ( transaction.type == "Expense" ) {
+                return  <div className="transaction debit" key={ transaction.id }>
+                          <span className="transaction-name">
+                            { transaction.desc }
+                          </span>
 
-            <span className="transaction-amount">
-              $3000
-            </span>
-          </div>
-          <div className="transaction credit">
-            <span className="transaction-name">
-              Salary
-            </span>
+                          <span className="transaction-amount">
+                            ${ transaction.amount }
+                          </span>
+                        </div>
+              } else if ( transaction.type == "Income" ) {
+                return <div className="transaction credit" key={ transaction.id }>
+                          <span className="transaction-name">
+                            { transaction.desc }
+                          </span>
 
-            <span className="transaction-amount">
-              $3000
-            </span>
-          </div>
-          <div className="transaction debit">
-            <span className="transaction-name">
-              food
-            </span>
+                          <span className="transaction-amount">
+                            ${ transaction.amount }
+                          </span>
+                        </div>
+              }
+            })
+          }
+          
+          {/* check if transaction data is empty */}
+          {
+            ( transactions.length == 0 && searchValue == "" ) && <h2 className="display-text">
+              You have no transactions for now
+            </h2>
+          }
 
-            <span className="transaction-amount">
-              $2000
-            </span>
-          </div>
+          {/* check if use is searching for some type of transaction in history and */}
+          {/* display results based on their search */}
+          {
+            // firstly filter out transactions that contain the searchValue from the transactions data
+            ( searchValue != "" && transactions.length != 0 ) && transactions.filter( function( transaction ) {
+
+              // filter condition checking if a transaction contains that 
+              // string in it's desc
+              if ( transaction.desc.includes( searchValue ) ) {
+                return true;
+              }
+            })
+            
+            // render list of filtered transactions as elements for search results
+            .map( function( transaction ) {
+              // check transaction type and return appropriate jsx markup based on the type
+              if ( transaction.type == "Expense" ) {
+                return  <div className="transaction debit" key={ transaction.id }>
+                          <span className="transaction-name">
+                            { transaction.desc }
+                          </span>
+
+                          <span className="transaction-amount">
+                            ${ transaction.amount }
+                          </span>
+                        </div>
+              } else if ( transaction.type == "Income" ) {
+                return <div className="transaction credit" key={ transaction.id }>
+                          <span className="transaction-name">
+                            { transaction.desc }
+                          </span>
+
+                          <span className="transaction-amount">
+                            ${ transaction.amount }
+                          </span>
+                        </div>
+              }
+            })
+          }
         </div>
       </div>
       
